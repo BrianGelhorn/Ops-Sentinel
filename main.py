14e3238895f8cb7e3bbab4989feb88981bf94206
Brian Desktop <briangelhorn@gmail.com>
2026-04-14 01:40:02 -0300

    Add datetime and status fields, update post_incidents logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 
@@ -9,49 +10,66 @@ async def get_health():
 
 @app.get("/ready")
 async def get_ready():
-    #TODO: Give funcionality to the ready depending the status of the api
+    #TODO: Give funcionality to the ready depending the status of the api and database
     return {"status": "ready"}
 
-INCIDENTS=[{"id": 1}, {"id": 2}]
+#TODO: Implement the SQLConnection
+INCIDENTS=[]
 
 @app.get("/incidents")
 async def get_incidents():
-    #TODO: Implement the SQLConnection
     return INCIDENTS
 
-class Trigger():
+class Trigger(BaseModel):
     type: str
     expected_status: int
     observed_status: int
     failed_attempts: int
 
-class Evidence():
+class Evidence(BaseModel):
     response_time_in_ms: int
     cpu_usage_percent: float
     memory_usage_percent: float
     error_message: str
 
-class Resolution():
+class Resolution(BaseModel):
     action_taken: str
     action_result: str
     date: str
 
-class Item(BaseModel):
-    id: int
+class CreateItem(BaseModel):
     title: str
     service: str
     type: str
     severity: str
     summary: str
-    date: str
     source: str
     trigger: Trigger
     evidence: Evidence
     resolution: Resolution
 
+class Item(CreateItem):
+    id: int
+    date: datetime
+    status: str
 
 
-@app.post("/incidents/")
-async def post_incidents(item: Item):
-    INCIDENTS.append({"id": item.id})
+
+@app.post("/incidents/", response_model=Item, status_code=status.HTTP_201_CREATED)
+async def post_incidents(itemToAdd: CreateItem):
+    item = Item(
+        id=1,
+        title=itemToAdd.title,
+        service=itemToAdd.service,
+        type=itemToAdd.type,
+        severity=itemToAdd.severity,
+        summary=itemToAdd.summary,
+        source=itemToAdd.source,
+        trigger=itemToAdd.trigger,
+        evidence=itemToAdd.evidence,
+        resolution=itemToAdd.resolution,
+        date=datetime.now(),
+        status="open"
+    )
+    INCIDENTS.append(item)
     return item
