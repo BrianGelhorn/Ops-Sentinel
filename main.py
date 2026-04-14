59041713da3d7e33fbb5fcfaeb0d7d227b6f5b90
Brian Desktop <briangelhorn@gmail.com>
2026-04-14 00:31:57 -0300

    Add GET /ready endpoint

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/health")
 async def get_health():
     return {"status": "alive"}
+
+@app.get("/ready")
+async def get_ready():
+    #TODO: Give funcionality to the ready depending the status of the api
+    return {"status": "ready"}
