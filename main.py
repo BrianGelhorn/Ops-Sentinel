a495150d42b615739b6fb7beb6419e20da09fd7a
Brian Desktop <briangelhorn@gmail.com>
2026-04-14 01:05:15 -0300

    Add GET /incidents endpoint

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ async def get_health():
 async def get_ready():
     #TODO: Give funcionality to the ready depending the status of the api
     return {"status": "ready"}
+
+@app.get("/incidents")
+async def get_incidents():
+    #TODO: Implement the SQLConnection
+    #TODO: Define the structure on the incidents
+    return [{"id": 1}, {"id": 2}]
