4027814c8370787b3d0704826883dfabc574c8a6
Brian Desktop <briangelhorn@gmail.com>
2026-04-14 01:24:09 -0300

    Incident Structure Defined

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,8 +12,46 @@ async def get_ready():
     #TODO: Give funcionality to the ready depending the status of the api
     return {"status": "ready"}
 
+INCIDENTS=[{"id": 1}, {"id": 2}]
+
 @app.get("/incidents")
 async def get_incidents():
     #TODO: Implement the SQLConnection
-    #TODO: Define the structure on the incidents
-    return [{"id": 1}, {"id": 2}]
+    return INCIDENTS
+
+class Trigger():
+    type: str
+    expected_status: int
+    observed_status: int
+    failed_attempts: int
+
+class Evidence():
+    response_time_in_ms: int
+    cpu_usage_percent: float
+    memory_usage_percent: float
+    error_message: str
+
+class Resolution():
+    action_taken: str
+    action_result: str
+    date: str
+
+class Item(BaseModel):
+    id: int
+    title: str
+    service: str
+    type: str
+    severity: str
+    summary: str
+    date: str
+    source: str
+    trigger: Trigger
+    evidence: Evidence
+    resolution: Resolution
+
+
+
+@app.post("/incidents/")
+async def post_incidents(item: Item):
+    INCIDENTS.append({"id": item.id})
+    return item
