aa4d31c122af545327b7eb871c38915c5131f17e
Brian Desktop <briangelhorn@gmail.com>
2026-04-18 15:14:46 -0300

    Add GET endpoint for retrieving all monitors and update imports

--- routers/monitors.py
+++ routers/monitors.py
@@ -1,7 +1,8 @@
 from fastapi.routing import APIRouter
 from schemas.monitor import MonitorCreate, MonitorResponse
 from services.monitor_service import create_monitor
-from database.crud import upload_to_database
+from database.crud import upload_to_database, get_all_from_database
+from database.dbmodels import Monitor
 import services.check_runner_service
 router = APIRouter()
 
@@ -12,3 +13,8 @@ async def post_monitor(monitor: MonitorCreate):
     services.check_runner_service.run_monitor_check(monitordb.id)
     response = MonitorResponse(**monitor.model_dump(), id=monitordb.id)
     return response
+
+@router.get("/monitor", response_model=list[MonitorResponse])
+async def get_all_monitors():
+    monitors = get_all_from_database(Monitor)
+    return monitors
