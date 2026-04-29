56fbe682f29152d9d4d4abfaf6e238f61afde44f
Brian Desktop <briangelhorn@gmail.com>
2026-04-29 13:20:26 -0300

    Refactor code for improved readability in run_monitor_check function

--- services/check_runner_service.py
+++ services/check_runner_service.py
@@ -30,11 +30,14 @@ async def run_monitor_check(monitorid: int):
             response = await client.get(monitor.config.url)
 
     except RequestError as e:
-        incident: Incident = next((inc 
-                                   for inc in other_incidents 
-                                   if (inc.monitor_id == monitor.id 
-                                       and inc.trigger.observed_status is None)
-                                       ), None)
+        incident: Incident = next(
+                                (
+                                    inc 
+                                    for inc in other_incidents 
+                                    if inc.monitor_id == monitor.id 
+                                    and inc.trigger.observed_status is None
+                                ), 
+                                None)
         if incident is None:
             incident = create_incident(IncidentCreate(
                 monitor_id=monitorid,
