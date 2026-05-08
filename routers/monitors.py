387cfc65dfc9df0a02daa66605d30103b5a8ff86
Brian Gelhorn <briangelhorn@gmail.com>
2026-05-07 22:59:12 -0300

    Fixed prefix path issue

--- routers/monitors.py
+++ routers/monitors.py
@@ -7,10 +7,10 @@ from database.crud import upload_to_database, get_all_from_database
 from database.dbmodels import Incident, Monitor
 from database.dbconection import get_db
 from schemas.incident import IncidentResponse
-router = APIRouter(prefix="/monitor/")
+router = APIRouter(prefix="/monitor")
 
 
-@router.post("", response_model=MonitorResponse)
+@router.post("/", response_model=MonitorResponse)
 async def post_monitor(monitor: MonitorCreate, db: Session = Depends(get_db)):
     monitordb = create_monitor(monitor)
     upload_to_database(monitordb, db)
@@ -18,13 +18,13 @@ async def post_monitor(monitor: MonitorCreate, db: Session = Depends(get_db)):
     return response
 
 
-@router.get("", response_model=list[MonitorResponse])
+@router.get("/", response_model=list[MonitorResponse])
 async def get_all_monitors(db: Session = Depends(get_db)):
     monitors = get_all_from_database(Monitor, db)
     return monitors
 
 
-@router.get("get_active_incidents", response_model=list[IncidentResponse])
+@router.get("/get_active_incidents", response_model=list[IncidentResponse])
 async def get_active_incidents_for_monitor(monitorid: int, db: Session = Depends(get_db)):
     return (
         db.query(Incident)
