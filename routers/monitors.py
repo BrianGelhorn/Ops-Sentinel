e118f941ab5bc32dd45d78c318aec7d9ab74ac88
Brian Gelhorn <briangelhorn@gmail.com>
2026-05-07 22:55:23 -0300

    Added get_active_incidents endpoint to monitor router to get incidents detected by a determined monitor

--- routers/monitors.py
+++ routers/monitors.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import Session
 from schemas.monitor import MonitorCreate, MonitorResponse
 from services.monitor_service import create_monitor
 from database.crud import upload_to_database, get_all_from_database
-from database.dbmodels import Monitor
+from database.dbmodels import Incident, Monitor
 from database.dbconection import get_db
-router = APIRouter()
+from schemas.incident import IncidentResponse
+router = APIRouter("/monitor/")
 
 
-@router.post("/monitor", response_model=MonitorResponse)
+@router.post("", response_model=MonitorResponse)
 async def post_monitor(monitor: MonitorCreate, db: Session = Depends(get_db)):
     monitordb = create_monitor(monitor)
     upload_to_database(monitordb, db)
@@ -17,7 +18,15 @@ async def post_monitor(monitor: MonitorCreate, db: Session = Depends(get_db)):
     return response
 
 
-@router.get("/monitor", response_model=list[MonitorResponse])
+@router.get("", response_model=list[MonitorResponse])
 async def get_all_monitors(db: Session = Depends(get_db)):
     monitors = get_all_from_database(Monitor, db)
     return monitors
+
+@router.get("get_active_incidents", response_model=list[IncidentResponse])
+async def get_active_incidents_for_monitor(monitorid: int, db: Session = Depends(get_db)):
+    return (
+        db.query(Incident)
+        .filter(Incident.monitor_id == monitorid, Incident.status != "resolved")
+        .all()
+    )
